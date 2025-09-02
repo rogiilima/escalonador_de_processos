065cfdbc78bcaf7bdfdaf8f1ce6ba5b7e76e2ddb
João <joaosobramatheus@gmail.com>
2025-09-02 09:09:25 -0300

    Foi realizado a estruturação básica do projeto e suas respectivas classes, também a mudança de app para main

---
+++ scheduler/src/Processos.java
@@ -0,0 +1,8 @@
+// classe que gerencia os processos
+public class Processos {
+    public int id;
+    public String nome;
+    public int prioridade; // 1 para alta, 2 para média e 3 para baixa
+    public int ciclos_necessarios;
+    public String recurso_necessario; // CPU, IO, etc
+}
---
+++ scheduler/src/Schedule.java
@@ -0,0 +1,15 @@
+// classe que gerencia o escalonador de processos
+public class Schedule {
+    
+    ListasDeProcessos lista_alta_prioridade = new ListasDeProcessos();
+    ListasDeProcessos lista_media_prioridade = new ListasDeProcessos();
+    ListasDeProcessos lista_baixa_prioridade = new ListasDeProcessos();
+    ListasDeProcessos  lista_bloqueados = new ListasDeProcessos();
+
+    int _ciclo_alta_prioridadde = 0;
+
+    
+    void executarCicloDeCPU() {
+
+    }
+}
