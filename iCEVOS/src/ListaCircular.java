404c3a00637b6e53863644eeb95f83b2ac880020
Igor Lima <igor.pflima@gmail.com>
2025-09-12 13:55:38 -0300

    fix: bugs da lista circular corrigidos

--- iCEVOS/src/ListaCircular.java
+++ iCEVOS/src/ListaCircular.java
@@ -4,7 +4,7 @@ public class ListaCircular {
 
     //Verifica se a lista está vazia
     public boolean listaVazia(){
-        if (tail == null) {
+        if (tail != null) {
             return false;
         }
         return true; 
@@ -26,11 +26,13 @@ public class ListaCircular {
     public void inserirListaVazia(Object objeto){
         Node node = new Node(objeto);
         if (listaVazia()) { //Se a lista estiver vazia
-            node.setNextNode(node);// Faz o nó apontar para ele mesmo
+            tail = node; //Define a calda
+            // Faz o nó apontar para ele mesmo
             tail.setNextNode(node); // Define a cauda como sendo o nó
             size++;
         }
     }
+
     public void inserirNoFim(Object objeto){
         //IF guard rail
         if (listaVazia()) {
@@ -40,7 +42,7 @@ public class ListaCircular {
         // Logica para inserir no fim(cauda)
         Node node = new Node(objeto);
         
-        node.setNextNode(tail);
+        node.setNextNode(tail.getNextNode());
         tail.setNextNode(node);
         tail = node;
         size++;
@@ -56,14 +58,20 @@ public class ListaCircular {
         if (tail.getNextNode() == tail) {
             Node rtnNode = tail;
             tail = null;
+            size--;
             return rtnNode;
         }else{
             //----Jogar exception ----
-            System.out.println("A lista não está vazia!");
+            System.out.println("A lista não é unitaria!");
             return null;
         }
     }
     public Node removeCabeca(){
+       if (listaVazia()) {
+            // -- Implementar exception
+            System.out.println("A lista está vazia!");
+            return null;
+       }
         if (size == 1) {
             return removeListaUnica();
         }
@@ -84,20 +92,23 @@ public class ListaCircular {
             return removeListaUnica();
         }
 
-        //Algoritmo ( existe outra maneira sem ser com dois nós, apenas percorrendo a lista!!!)
-        //Nós auxiliares
-        Node auxNode1 = tail;
-        Node auxNode2 = tail.getNextNode();
-        do { // Percorre os nós
-            auxNode1 = auxNode1.getNextNode();
-            auxNode2 = auxNode2.getNextNode();
-        } while (auxNode1 != tail);
+        //Algoritmo ( existe outra maneira com dois nós, apenas percorrendo a lista!!!)
 
+        Node auxNode = tail.getNextNode();//Nós auxiliares, começando pela "head"
+        // Percorre até encontrar o penultimo nó
+        while (auxNode.getNextNode() != tail);{ 
+            auxNode = auxNode.getNextNode();
+        }
+
+        Node rtnNode = tail;
         //remove a cauda
-        auxNode2 = auxNode1.getNextNode();
-        tail = auxNode2;
+        auxNode.setNextNode(tail.getNextNode());//continua a lista
+
+        tail = auxNode;
+        
+        rtnNode.setNextNode(null);// Garantir que o GC apague ele depois
         size--;
-        return auxNode1;// Retorna a cauda;
+        return rtnNode;// Retorna a cauda;
 
     }
 }
