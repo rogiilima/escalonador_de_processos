56bef2b387c5a1d9fee9d7852cfaca834912beae
João <joaosobramatheus@gmail.com>
2025-09-13 19:40:00 -0300

    Adicionando lógica de anti-inanição do sheduler

--- iCEVOS/src/Scheduler.java
+++ iCEVOS/src/Scheduler.java
@@ -1,5 +1,5 @@
 public class Scheduler {
-    // Três listas encadeadas simples (uma para cada prioridade)
+    // Três listas encadeadas simples
     private ListasDeProcessos listaAlta = new ListasDeProcessos();
     private ListasDeProcessos listaMedia = new ListasDeProcessos();
     private ListasDeProcessos listaBaixa = new ListasDeProcessos();
@@ -31,10 +31,22 @@ public class Scheduler {
 
         // Desbloqueio - pega o mais antigo da lista circular
         if (!listaBloqueados.listaVazia()) {
-            EstruturaProcesso desbloqueado = listaBloqueados.removeCabeca();
+            EstruturaProcesso desbloqueado = listaBloqueados.removeCabeca(); 
             System.out.println("Desbloqueando " + desbloqueado.getNome());
             inserirProcesso(desbloqueado);
         }
-
+        EstruturaProcesso atual = null;
+        if (contadorAlta == 5) { // se o contadorAlta for igual a 5, remove cabeça de lista media, executando o processo
+        atual = listaMedia.removeCabeca();
+        
+        if (atual == null) { // se não houver lista media tentar de baixa
+        atual = listaBaixa.removeCabeca();
+        } 
+    } 
+    if (atual != null) { // faz a anti-inanição do scheduler
+                System.out.println("Anti-inanição aplicada! Executando " + atual.getNome());
+                contadorAlta = 0;
+                return;
+            }
     }
 }
