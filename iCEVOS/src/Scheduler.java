1cb4645beb566492a2f9e0510064c0947d955a86
Igor Lima <igor.pflima@gmail.com>
2025-09-12 22:44:16 -0300

    alteracao no nome da classe scheduler

--- iCEVOS/src/Scheduler.java
+++ iCEVOS/src/Scheduler.java
@@ -1,16 +1,16 @@
 // classe que gerencia o escalonador de processos
-public class Schedule {
+public class Scheduler {
     public static void main(String[] args) throws Exception {
     ListasDeProcessos lista_alta_prioridade = new ListasDeProcessos();
     ListasDeProcessos lista_media_prioridade = new ListasDeProcessos();
     ListasDeProcessos lista_baixa_prioridade = new ListasDeProcessos();
     ListasDeProcessos lista_bloqueados = new ListasDeProcessos();
 
-    int _ciclo_alta_prioridadde = 0;
+    int ciclo_alta_prioridadde = 0;
 
     
-    // void executarCicloDeCPU() {
+    //public void executarCicloDeCPU() {
     // }
-    
+
     }
 }
